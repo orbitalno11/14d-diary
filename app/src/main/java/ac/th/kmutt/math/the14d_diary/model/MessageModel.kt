a174f61f31aafad5fa6f79dafb2bacb74f60d197
orbitalno11 <43870591+orbitalno11@users.noreply.github.com>
2020-04-12 18:49:32 +0700

    add edit message model

--- app/src/main/java/ac/th/kmutt/math/the14d_diary/model/MessageModel.kt
+++ app/src/main/java/ac/th/kmutt/math/the14d_diary/model/MessageModel.kt
@@ -1,9 +1,23 @@
 package ac.th.kmutt.math.the14d_diary.model
 
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 class MessageModel(
     var sender: String = "",
     var senderID: String = "",
     var message: String = "",
     var sendTime: String = ""
 ) {
+
+    @Exclude
+    fun toMap(): Map<String, String>{
+        return mapOf(
+            "sender" to sender,
+            "senderID" to senderID,
+            "message" to message,
+            "sendTime" to sendTime
+        )
+    }
 }
